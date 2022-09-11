40d6d380b8801eb51347e96226253f58b5d96032
Artem Holovchenko <golovchenkoart@gmail.com>
2022-09-11 21:05:50 +0200

    Spring Boot Config Server added

---
+++ configserver/src/main/java/art/holovchenko/onlinestore/configserver/ConfigurationServerApplication.java
@@ -0,0 +1,15 @@
+package art.holovchenko.onlinestore.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServerApplication.class, args);
+	}
+
+}
